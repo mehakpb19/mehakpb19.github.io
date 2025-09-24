4ade408a2cd48f669bd2c23afa9dd0d9b1d76c0b
mehakpree <prince56studio@gmail.com>
2025-09-24 16:45:31 +0530

    adsf

---
+++ Mehak Important!/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './app.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div class="bg-amber-300">
+        falks;alfk
+      </div>
+    );
+  }
+}
+
+export default App;
